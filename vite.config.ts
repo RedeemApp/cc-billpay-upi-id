aabb738f298c2a3848106204b68506ba8a00dbe6
mkagenius <mkagenius@gmail.com>
2024-09-17 23:55:36 +0530

    change base to repo name for js paths to be correct in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cc-billpay-upi-id/',
+  base: '/cc-billpay-upi-id/', // need to remove for dev or use import.meta.env.PROD ?
   build: {
     outDir: 'docs', // Output to root directory
 
