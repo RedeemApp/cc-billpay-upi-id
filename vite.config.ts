5cbb5a4970733e3800b4e834ee387d27439163ec
mkagenius <mkagenius@gmail.com>
2024-09-17 23:47:22 +0530

    change base to docs for js paths to be correct in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/docs/',
   build: {
     outDir: 'docs', // Output to root directory
+
   },
   publicDir: 'public'
 })
